03ba39cb93ac1a87da5b68a1c17fef7fe7e910a1
Lucifer <10078186+luc1fev@users.noreply.github.com>
2022-05-04 08:21:11 -0700

    更新写法

--- GUI/web_gui/views.py
+++ GUI/web_gui/views.py
@@ -128,8 +128,8 @@ def main_view(request, actions, job_title):
         dire = director()
         value = dire.view_staff_report()
         cont = {
-            'actions': 'view',
-            'job_title': 'DIRECTOR',
+            'actions': actions,
+            'job_title': job_title,
             'animal_h': value[0],
             'animal_r': value[1],
             'manager_h': value[2],
